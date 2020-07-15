e0ae914b51cd3f2e45a286e6a13af34652f1a208
mahamedbelkeir <mohamedbel@protonmail.com>
2020-07-15 21:38:17 +0200

    added poppins font and made a WIP navbar component

---
+++ components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav class="navbar">
+      <ul class="nav-items">
+          <li>About</li>
+          <li>Experience</li>
+          <li>Projects</li>
+          <li>Blog</li>
+          <li>Contact</li>
+      </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.navbar {
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+    margin: 10px;
+}
+
+.nav-items {
+    display: flex;
+    width: 30%;
+    justify-content: space-around;
+    list-style: none;
+    margin-right: 10% ;
+}
+
+li {
+    margin: 10px;
+}
+</style>
